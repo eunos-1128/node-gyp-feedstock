c49fb1df010be416d62996ac4f842c54dbdb2914
UENO, M. <eunosium-1128@live.jp>
2026-05-22 01:14:03 +0900

    Update build.bat to handle architecture-specific npm config

--- recipe/build.bat
+++ recipe/build.bat
@@ -6,6 +6,7 @@ if "%target_platform%"=="win-64" (
 ) else if "%target_platform%"=="win-arm64" (
     set "npm_config_arch=arm64"
 )
+if errorlevel 1 exit /b 1
 
 @REM Don't use pre-built gyp packages
 set "npm_config_build_from_source=true"
@@ -13,7 +14,10 @@ set "NPM_CONFIG_USERCONFIG=%TEMP%\nonexistentrc"
 
 @REM Replace node symlink with BUILD_PREFIX version
 del "%PREFIX%\bin\node.exe"
+if errorlevel 1 exit /b 1
+
 mklink "%PREFIX%\bin\node.exe" "%BUILD_PREFIX%\bin\node.exe"
+if errorlevel 1 exit /b 1
 
 pnpm install --prod
 if errorlevel 1 exit /b 1
